fa92dcac49b8cc6fa1ceb3eb09751a86638b4feb
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-01-10 19:17:22 +0100

    [chore]: Improve the load-balancer handle unenabling the access to each service from external

--- src/main/java/com/nmincuzzi/service/DummyService.java
+++ src/main/java/com/nmincuzzi/service/DummyService.java
@@ -2,15 +2,22 @@ package com.nmincuzzi.service;
 
 import com.nmincuzzi.exception.DummyBadRequestException;
 import com.nmincuzzi.model.DummyModel;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class DummyService {
 
+    Logger logger = LoggerFactory.getLogger(DummyService.class);
+
     public DummyModel retrieveDummyInfo() {
+        logger.info("Retrieve dummy info by Service.");
         DummyModel dummyModel = DummyModel.of();
         dummyModel.setId(UUID.randomUUID().toString());
         dummyModel.setCode(HttpStatus.OK.value());
